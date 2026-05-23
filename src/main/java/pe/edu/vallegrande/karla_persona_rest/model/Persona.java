53321d56e763aab259231640e42e4e24cd8256b9
Karla Anyelí Magallanes Vicente <karla.magallanes.vicente@vallegrande.edu.pe>
2026-05-23 03:13:51 +0000

    fix: correct table name in the Person class to uppercase

--- src/main/java/pe/edu/vallegrande/karla_persona_rest/model/Persona.java
+++ src/main/java/pe/edu/vallegrande/karla_persona_rest/model/Persona.java
@@ -11,7 +11,7 @@ import org.springframework.data.relational.core.mapping.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table("personas")
+@Table("PERSONAS")
 public class Persona {
     @Id
     private Long id;
